b6df886c539b5f4adedaef362afb917ed9a3048c
Chris Chapman <38057494+SeattleChris@users.noreply.github.com>
2025-12-13 20:57:27 -0800

    Refactor code.py by removing unused imports
    Removed unused imports and cleaned up code formatting.

--- code.py
+++ code.py
@@ -1,10 +1,6 @@
 #!/bin/python3
 
-import math
 import os
-import random
-import re
-import sys
 
 class SinglyLinkedListNode:
     def __init__(self, node_data):
@@ -19,22 +15,12 @@ class SinglyLinkedList:
 def print_singly_linked_list(node, sep, fptr):
     while node:
         fptr.write(str(node.data))
-
         node = node.next
-
         if node:
             fptr.write(sep)
 
 # Complete the insertNodeAtHead function below.
 
-#
-# For your reference:
-#
-# SinglyLinkedListNode:
-#     int data
-#     SinglyLinkedListNode next
-#
-#
 def insertNodeAtHead(prev, data):
     curr = SinglyLinkedListNode(data)
     curr.next = prev
@@ -42,18 +28,13 @@ def insertNodeAtHead(prev, data):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     llist_count = int(input())
-
     llist = SinglyLinkedList()
-
     for _ in range(llist_count):
         llist_item = int(input())
         llist_head = insertNodeAtHead(llist.head, llist_item)
         llist.head = llist_head
-    
     print_singly_linked_list(llist.head, '\n', fptr)
     fptr.write('\n')
-    
     fptr.close()
 
